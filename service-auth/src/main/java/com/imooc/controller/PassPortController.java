a04025f1cbfff00bc5f9671dcc715c9d1a44caca
liuao <1161274856@qq.com>
2023-04-05 17:37:27 +0800

    SpringBoot集成腾讯云短信 以及 整合redis存储验证码2

--- service-auth/src/main/java/com/imooc/controller/PassPortController.java
+++ service-auth/src/main/java/com/imooc/controller/PassPortController.java
@@ -3,8 +3,10 @@ package com.imooc.controller;
 
 import com.imooc.base.BaseInfoProperties;
 import com.imooc.grace.result.GraceJSONResult;
+import com.imooc.utils.IPUtil;
 import com.imooc.utils.SMSUtils;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -30,6 +32,14 @@ public class PassPortController extends BaseInfoProperties {
 
     @GetMapping("getSMSCode")
     public GraceJSONResult getSNSCode(String mobile, HttpServletRequest request) throws Exception {
+
+        if (StringUtils.isBlank(mobile)) {
+            return GraceJSONResult.errorMsg("手机号信息不不能为空");
+        }
+        // 限制用户在60s内只能获得一次验证码
+        String userIp = IPUtil.getRequestIp(request);
+        log.info("userIp值：" + userIp);
+        redis.setnx60s(MOBILE_SMSCODE + ":" + userIp, mobile);
 
         String code = (int)((Math.random() * 9 + 1) * 100000) + "";
         log.info("验证码code值：" + code);
