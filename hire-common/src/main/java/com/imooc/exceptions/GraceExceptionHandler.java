c2da7e487f86b152df9a62da6387cff6ed5d20b2
liuao <1161274856@qq.com>
2023-04-07 21:21:58 +0800

    SpringBoot集成腾讯云短信 拦截器异常抛出优雅封装

---
+++ hire-common/src/main/java/com/imooc/exceptions/GraceExceptionHandler.java
@@ -0,0 +1,84 @@
+package com.imooc.exceptions;
+
+import com.imooc.grace.result.GraceJSONResult;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@ControllerAdvice
+public class GraceExceptionHandler {
+
+//    @ExceptionHandler(ArithmeticException.class)
+//    @ResponseBody
+//    public GraceJSONResult returnArithmeticException(ArithmeticException e) {
+//        e.printStackTrace();
+//        return GraceJSONResult.errorMsg(e.getMessage());
+//    }
+
+//    @ExceptionHandler(KeeperException.BadVersionException.class)
+//    @ResponseBody
+//    public GraceJSONResult returnBadVersionException(KeeperException.BadVersionException e) {
+//        e.printStackTrace();
+//        return GraceJSONResult.exception(ResponseStatusEnum.ZOOKEEPER_BAD_VERSION_ERROR);
+//    }
+
+//    @ExceptionHandler(MaxUploadSizeExceededException.class)
+//    @ResponseBody
+//    public GraceJSONResult returnMaxUploadSizeExceededException(MaxUploadSizeExceededException e) {
+//        e.printStackTrace();
+//        return GraceJSONResult.exception(ResponseStatusEnum.FILE_MAX_SIZE_500KB_ERROR);
+//    }
+
+    @ExceptionHandler(MyCustomException.class)
+    @ResponseBody
+    public GraceJSONResult returnMyCustomException(MyCustomException e) {
+        e.printStackTrace();
+        return GraceJSONResult.exception(e.getResponseStatusEnum());
+    }
+
+//    @ExceptionHandler({
+//            SignatureException.class,
+//            ExpiredJwtException.class,
+//            UnsupportedJwtException.class,
+//            MalformedJwtException.class,
+//            io.jsonwebtoken.security.SignatureException.class
+//    })
+//    @ResponseBody
+//    public GraceJSONResult returnSignatureException(SignatureException e) {
+//        e.printStackTrace();
+//        return GraceJSONResult.exception(ResponseStatusEnum.JWT_SIGNATURE_ERROR);
+//    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseBody
+    public GraceJSONResult returnNotValidException(MethodArgumentNotValidException e) {
+        BindingResult result = e.getBindingResult();
+        Map<String, String> errors = getErrors(result);
+        return GraceJSONResult.errorMap(errors);
+    }
+
+    public Map<String, String> getErrors(BindingResult result) {
+
+        Map<String, String> map = new HashMap<>();
+
+        List<FieldError> errorList = result.getFieldErrors();
+        for (FieldError fe : errorList) {
+            // 错误所对应的属性字段名
+            String field = fe.getField();
+            // 错误信息
+            String message = fe.getDefaultMessage();
+
+            map.put(field, message);
+        }
+
+        return map;
+    }
+
+}
