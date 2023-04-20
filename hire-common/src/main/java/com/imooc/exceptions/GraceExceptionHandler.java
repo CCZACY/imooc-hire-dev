57d5774df2587f5ff0210ddc46c9d6d051a9cd37
liuao <1161274856@qq.com>
2023-04-20 21:48:20 +0800

    SAAS企业后台 扫码登录获

--- hire-common/src/main/java/com/imooc/exceptions/GraceExceptionHandler.java
+++ hire-common/src/main/java/com/imooc/exceptions/GraceExceptionHandler.java
@@ -1,6 +1,10 @@
 package com.imooc.exceptions;
 
 import com.imooc.grace.result.GraceJSONResult;
+import com.imooc.grace.result.ResponseStatusEnum;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.MalformedJwtException;
+import io.jsonwebtoken.UnsupportedJwtException;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -8,6 +12,7 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.security.SignatureException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -43,18 +48,18 @@ public class GraceExceptionHandler {
         return GraceJSONResult.exception(e.getResponseStatusEnum());
     }
 
-//    @ExceptionHandler({
-//            SignatureException.class,
-//            ExpiredJwtException.class,
-//            UnsupportedJwtException.class,
-//            MalformedJwtException.class,
-//            io.jsonwebtoken.security.SignatureException.class
-//    })
-//    @ResponseBody
-//    public GraceJSONResult returnSignatureException(SignatureException e) {
-//        e.printStackTrace();
-//        return GraceJSONResult.exception(ResponseStatusEnum.JWT_SIGNATURE_ERROR);
-//    }
+    @ExceptionHandler({
+            SignatureException.class,
+            ExpiredJwtException.class,
+            UnsupportedJwtException.class,
+            MalformedJwtException.class,
+            io.jsonwebtoken.security.SignatureException.class
+    })
+    @ResponseBody
+    public GraceJSONResult returnSignatureException(SignatureException e) {
+        e.printStackTrace();
+        return GraceJSONResult.exception(ResponseStatusEnum.JWT_SIGNATURE_ERROR);
+    }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     @ResponseBody
--- service-auth/src/main/java/com/imooc/controller/SaasPassportController.java
+++ service-auth/src/main/java/com/imooc/controller/SaasPassportController.java
@@ -2,17 +2,26 @@ package com.imooc.controller;
 
 import com.imooc.base.BaseInfoProperties;
 import com.imooc.grace.result.GraceJSONResult;
+import com.imooc.grace.result.ResponseStatusEnum;
+import com.imooc.utils.JWTUtils;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.UUID;
 
 @RestController
 @RequestMapping("saas")
 @Slf4j
 public class SaasPassportController extends BaseInfoProperties {
+
+    @Autowired
+    private JWTUtils jwtUtils;
 
     @PostMapping("getQRToken")
     public GraceJSONResult getQRToken()  {
@@ -21,9 +30,43 @@ public class SaasPassportController extends BaseInfoProperties {
         String qrToken = UUID.randomUUID().toString();
         // 把token存入redis，设置有效时限，超时未扫码则刷新获得新的二维码
         redis.set(SAAS_PLATFORM_LOGIN_TOKEN + ":" + qrToken, qrToken, 5*60);
-        // 存入redis标记当前的qrToken未被扫描读取
+        // 存入redis标记，当前的qrToken未被扫描读取
         redis.set(SAAS_PLATFORM_LOGIN_TOKEN_READ + ":" + qrToken, "0", 5*60);
         // 返回给前端，并让前端下一次请求的时候带上qrToken
         return GraceJSONResult.ok(qrToken);
+    }
+
+    @PostMapping("scanCode")
+    public GraceJSONResult scanCode(@RequestParam String qrToken, HttpServletRequest request) {
+
+        // 判空 qrToken
+        if (StringUtils.isBlank(qrToken)) {
+            return GraceJSONResult.errorCustom(ResponseStatusEnum.FAILED);
+        }
+        // 从redis中获得并判断qrToken是否有效
+        String redisQRToken = redis.get(SAAS_PLATFORM_LOGIN_TOKEN + ":" + qrToken);
+        if (!redisQRToken.equalsIgnoreCase(qrToken)) {
+            return GraceJSONResult.errorCustom(ResponseStatusEnum.FAILED);
+        }
+        // 获得用户id以及jwt令牌
+        String appUserId = request.getHeader("appUserId");
+        String appUserToken = request.getHeader("appUserToken");
+        // 判空 用户id 和 用户jwt令牌
+        if (StringUtils.isBlank(appUserId) || StringUtils.isBlank(appUserToken)) {
+            return GraceJSONResult.errorCustom(ResponseStatusEnum.HR_TICKET_INVALID);
+        }
+        // 校验jwt
+        String userJson = jwtUtils.checkJWT(appUserToken.split("@")[1]);
+        if (StringUtils.isBlank(userJson)) {
+            return GraceJSONResult.errorCustom(ResponseStatusEnum.HR_TICKET_INVALID);
+        }
+        // 执行后续正常业务 生成预登录token
+        String preToken = UUID.randomUUID().toString();
+        // 把预登录token存入redis，来替换qrToken，设置有效时限，超时未扫码则刷新获得新的二维码
+        redis.set(SAAS_PLATFORM_LOGIN_TOKEN + ":" + qrToken, preToken, 5*60);
+        // 存入redis标记，当前的qrToken需要被读取并且失效覆盖，网页端标记二维码已被扫
+        redis.set(SAAS_PLATFORM_LOGIN_TOKEN_READ + ":" + qrToken, "1,"+ preToken, 5*60);
+        // 返回给手机端 app下次请求携带preToken
+        return GraceJSONResult.ok(preToken);
     }
 }
